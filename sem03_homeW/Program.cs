21f55ad36a1eb78ff2f812637a5523c5580483cf
PrNatali <79377175544@ya.ru>
2022-09-15 17:46:38 +0300

    palindrome

---
+++ sem03_homeW/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+void Palindrome(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        int secondnum = number % 10;
+        result = result * 10 + secondnum;
+        number = number / 10;
+    }
+    if (number == result)
+        Console.WriteLine("number is palindrome");
+    else
+        Console.WriteLine("dont palindrome");   
+}
+
+Console.WriteLine("enter the number");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+Palindrome(x);
