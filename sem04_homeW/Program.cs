7feb5dca3e7c178a12e4916c8073e86b7ae11c66
PrNatali <79377175544@ya.ru>
2022-09-27 14:00:36 +0300

    example

---
+++ sem04_homeW/Program.cs
@@ -0,0 +1,19 @@
+﻿int Pow(int a, int b)
+{
+    int result = a;
+    for(int cur = 1; cur < b; cur ++)
+        result *= a;
+    return result;           
+}
+
+Console.WriteLine("enter number");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("enter power to the number");
+int y = Convert.ToInt32(Console.ReadLine());
+int res = Pow(x, y);
+Console.WriteLine(res);
+
+
+
+
