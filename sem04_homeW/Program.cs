2c23a21afacd9c5530940e968cc9c11e53e0db85
PrNatali <79377175544@ya.ru>
2022-09-28 22:32:20 +0300

    home

--- sem04_homeW/Program.cs
+++ sem04_homeW/Program.cs
@@ -38,3 +38,27 @@ Console.WriteLine("enter the number");
 int num = Convert.ToInt32(Console.ReadLine());
 SumDigits(num);
 */
+//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int [] NewArray(int x)
+{
+    int []arr = new int [x];
+    for (int i = 0; i < x; i++)
+    {
+        Console.Write($"input value index {i}: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+void PrintArray(int[]arr)
+{
+    Console.Write("[ ");
+    for(int i = 0; i< arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write("]");
+}
+Console.Write("enter quantity number in the array: ");
+int a = Convert.ToInt32(Console.ReadLine());
+PrintArray(NewArray(a));
